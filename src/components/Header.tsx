f70f58f7fdaca8bac788b8d92bbfd8cb818c71c9
sxivansx <shivanshp987@gmail.com>
2025-05-21 22:16:37 +0530

    minor fixes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -113,7 +113,7 @@ export const Header = () => {
                   />
                 </>
               )}
-              {routes["/blog"] && (
+              {/* {routes["/blog"] && (
                 <>
                   <ToggleButton
                     className="s-flex-hide"
@@ -129,7 +129,7 @@ export const Header = () => {
                     selected={pathname.startsWith("/blog")}
                   />
                 </>
-              )}
+              )} */}
               {routes["/gallery"] && (
                 <>
                   <ToggleButton
